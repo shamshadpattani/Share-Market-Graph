745ea038f42724789ac5ffa1bf40bf86190645a8
shamshadpattani <mail4shamshadpattani@gmail.com>
2020-12-28 14:15:02 +0530

    design changed

--- app/src/main/java/com/example/stockchart/ui/MainActivity.kt
+++ app/src/main/java/com/example/stockchart/ui/MainActivity.kt
@@ -10,13 +10,14 @@ import com.example.stockchart.R
 import com.example.stockchart.data.model.Dataset
 import com.example.stockchart.databinding.ActivityMainBinding
 import com.github.aachartmodel.aainfographics.aachartcreator.*
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
-
+    private val aaChartModel: AAChartModel = AAChartModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
@@ -45,11 +46,8 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setmap(dataset: List<Double>, date: List<String>) {
-        val aaChartModel: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Areaspline)
-            .title("TATA")
-            .subtitle("")
-            .backgroundColor("#00000")
+            aaChartModel.chartType(AAChartType.Areaspline)
+            .backgroundColor("#1c232e")
             .xAxisTickInterval(100)
             .borderRadius(0f)
             .yAxisGridLineWidth(0.001f)
@@ -60,21 +58,22 @@ class MainActivity : AppCompatActivity() {
             .animationDuration(2000)
             .animationType(AAChartAnimationType.EaseInSine)
             .series(
-                arrayOf(
-                    AASeriesElement()
-                        .name("aa")
-                        .data(dataset.toTypedArray())
-                        .color("#85c785")
-                        .fillOpacity(0.12f)
-                        .borderWidth(0f)
-                        .enableMouseTracking(true)
-                        .dashStyle(AAChartLineDashStyleType.Solid)
-                )
+                    arrayOf(AASeriesElement()
+                            .name("aa")
+                            .data(dataset.toTypedArray())
+                            .color("#85c785")
+                            .fillOpacity(0.12f)
+                            .borderWidth(0f)
+                            .enableMouseTracking(true)
+                            .dashStyle(AAChartLineDashStyleType.Solid))
             )
         aaChartView.aa_drawChartWithChartModel(aaChartModel)
     }
 
     private fun showTitles(data: Dataset) {
         binding.mainTitle.text = data.name
+        aaChartModel.title(data.name)
+                .titleStyle(AAStyle().color("#810000").fontWeight(AAChartFontWeightType.Bold))
+                .subtitle(data.description)
     }
 }
